cad1b584d558542bafff6d910ba685f9b6c4d19b
_asdx_ <xasdx@tuta.io>
2017-12-03 12:28:57 +0000

    Add http-ping

---
+++ http-ping.rb
@@ -0,0 +1,39 @@
+# An object that executes a GET on a collection of URLs.
+# Intended to wake up multiple sleeping dynos on heroku.
+# https://devcenter.heroku.com/articles/free-dyno-hours#dyno-sleeping
+
+require "typhoeus"
+
+class HttpPing
+  
+  def initialize(urls)
+    @urls = urls
+    @hydra = Typhoeus::Hydra.new
+  end
+  
+  def ping
+    @urls.each do |url|
+      puts "GET - #{url}"
+      res = Typhoeus.get(url)
+      puts "#{res.code} - #{url}"
+    end
+  end
+  
+  def ping_parallel
+    @urls.each do |url|
+      puts "GET - #{url}"
+      req = Typhoeus::Request.new(url)
+      req.on_complete do |res|
+        puts "#{res.code} - #{url}"
+      end
+      @hydra.queue(req)
+    end
+    @hydra.run
+  end
+end
+
+HttpPing.new([
+  "http://www.google.com",
+  "https://www.twitch.tv",
+  "https://www.youtube.com"
+]).ping_parallel
